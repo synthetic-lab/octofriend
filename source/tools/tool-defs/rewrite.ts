401224cef87b1d826df4f0997c8e9905bd5af4d9
Matt Baker <matthew.reiss.baker@gmail.com>
2026-04-27 16:53:56 -0700

    Remove unused import

--- source/tools/tool-defs/rewrite.ts
+++ source/tools/tool-defs/rewrite.ts
@@ -1,6 +1,6 @@
 import { t } from "structural";
 import { fileTracker } from "../file-tracker.ts";
-import { attemptUntrackedRead, defineTool, autoparse } from "../common.ts";
+import { attemptUntrackedRead, defineTool } from "../common.ts";
 
 const ArgumentsSchema = t.subtype({
   filePath: t.str.comment("The path to the file"),
