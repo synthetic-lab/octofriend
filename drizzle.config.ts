de78cb9f4d9dcccc454f3af481df096ebe168866
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-07-03 11:47:02 +0300

    remove

--- drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "drizzle-kit";
-
-export default {
-  schema: "./source/**/schema/*.ts",
-  out: "./drizzle",
-  dialect: "sqlite",
-  casing: "snake_case",
-} satisfies Config;
